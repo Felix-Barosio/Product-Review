f5dd9e59bc5ef82c7c44afd594ecd0d28c235e6c
Felix <barosiofelix@gmail.com>
2022-11-18 21:10:46 +0300

    Added seed data in reveiw

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,5 +23,11 @@ puts "Creating reviews..."
 # *****************************************************************
 # Create Reviews Here
 
+new_review1 = Review.create(star_rating: rand(1..5), comment: Faker::Lorem.sentence, user_id: user2.id, product_id: product5.id)
+new_review2 = Review.create(star_rating: rand(1..5), comment: Faker::Lorem.sentence, user_id: user1.id, product_id: product4.id)
+new_review3 = Review.create(star_rating: rand(1..5), comment: Faker::Lorem.sentence, user_id: user2.id, product_id: product2.id)
+new_review4 = Review.create(star_rating: rand(1..5), comment: Faker::Lorem.sentence, user_id: user1.id, product_id: product3.id)
+new_review5 = Review.create(star_rating: rand(1..5), comment: Faker::Lorem.sentence, user_id: user3.id, product_id: product1.id)
+
 
 puts "Seeding done!"

3ea6c0545a9b9f1d3a062bc2830e037cf5a415de
Felix <barosiofelix@gmail.com>
2022-11-18 16:49:24 +0300

    Added reveiws table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,15 @@ ActiveRecord::Schema.define(version: 2022_11_05_065339) do
     t.integer "price"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "star_rating"
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
